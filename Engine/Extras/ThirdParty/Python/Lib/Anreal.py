cb1a3d695a5fd22827a3e26b92add720f8a4a3c9
kiseop kim <kiseop91@naver.com>
2021-06-18 00:48:49 +0900

    [GENERATOR] Fix folder path extraction function bug (#3)
    * [GENERATOR] Fix folder path extraction function bug
    
    * delete print log
    
    * 코드리뷰 반영

--- Engine/Extras/ThirdParty/Python/Lib/Anreal.py
+++ Engine/Extras/ThirdParty/Python/Lib/Anreal.py
@@ -1,3 +1,21 @@
+import os
 
-def HelloWorld() :
-    return "Hello World"
+def GetAnrealRootDir() :
+    Ret = os.getcwd()
+    StrList = Ret.split('\\')
+    Rootindex = len(StrList) - 4
+    Index = 0
+    RootPath = ""
+    
+    for i in StrList:
+        if Index == Rootindex:
+            break
+        Slash = "/"
+        if Index == Rootindex- 1:
+            Slash = ""
+        RootPath += i + Slash
+        Index = Index + 1
+
+    Ret = RootPath
+    print(Ret)
+    return Ret
